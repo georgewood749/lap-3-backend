acf745771e06f0f8a88d946f54a92003b58d6028
georgewood749 <georgewood749@gmail.com>
2022-11-15 11:17:29 +0000

    updated .env and mongo connection

--- dbConfig.js
+++ dbConfig.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb')
 
 const init = async () => {
-    const client = await MongoClient.connect(DB_URI)
+    const client = await MongoClient.connect('mongodb+srv://admin:gBKAMaSoHndIzC1R@cluster0.0y9hxp2.mongodb.net/?retryWrites=true&w=majority')
     return client.db(dbName)
 }
 
